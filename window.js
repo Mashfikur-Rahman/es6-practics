f4302c76e430160ac26c8c01ba1bf6947f920c1e
Mashfikur-Rahman <md.mashfikur1@gmail.comecho>
2020-04-28 20:18:18 +0600

    updated

--- window.js
+++ window.js
@@ -1,12 +1,15 @@
 class Person {
-    constructor(firstName, lastName, salary) {
+    constructor(firstName, lastName, salary, idNumber) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.salary = salary;
+        this.idNumber = idNumber;
     }
 }
 
-const heroPerson = new Person('Tom', 'John', 202020);
+const heroPerson = new Person('Tom', 'John', 202020, '2020');
 console.log(heroPerson);
-const newMan = new Person('man', 'who', 10000);
+const newMan = new Person('man', 'who', 10000, '1010');
 console.log(newMan);
+const idNumber = new Person('mash', 'rah', 404000, '007');
+console.log(idNumber);
